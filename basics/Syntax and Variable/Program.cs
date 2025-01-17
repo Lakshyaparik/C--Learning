ca25b0d6503393675584567bad1e9bc74898d5ae
lakshya <lakshyaparik99@gmail.com>
2025-01-17 19:44:03 +0530

    first commit

---
+++ basics/Syntax and Variable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //.....output
+            // Console.WriteLine("Hello, World!");
+            // Console.ReadLine();
+
+            //variables
+            // int a=10;
+            // double abc=3333333;
+            // float just=30.30f;
+
+            // Console.WriteLine(a);
+            // Console.WriteLine(abc);
+            // Console.WriteLine(just);
+
+            // const string Name="lakshya";//we can't put value again and also intilizezation must
+            // Console.Write(Name);
+            // string sirname="parek";
+            // Console.WriteLine(sirname);
+            // sirname="sharma";
+            // Console.WriteLine(sirname);
+
+            //adding a  text and varibles
+            // string name="lakshya";
+            // Console.WriteLine("hello"+name);
+        }
+    }
+}
