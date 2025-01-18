64f375f7c47a5bba2dceb38dd5ff93d5b110ce48
lakshya <lakshyaparik99@gmail.com>
2025-01-18 23:37:40 +0530

    type casting,userInput,mathClasses

---
+++ basics/02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Abc{
+    public static void Main(string[] args)
+    {
+        //type casting
+
+        int a=10;
+        double b=a;  //implicit type casting
+        // Console.WriteLine(a);
+        // Console.WriteLine(b);
+
+        double num1=100;
+        int num2=(int)(num1);//explicit typecasting
+        //Console.WriteLine(num2);
+
+        //const bool ischeck=true;
+        //Console.WriteLine(Convert.ToString(ischeck));
+
+        //userinput
+
+        //Console.WriteLine("Enter your name");
+        //string name=Console.ReadLine();
+
+        //Console.WriteLine(name);
+
+        //Console.WriteLine("Enter your age");
+        //int age=Convert.ToInt32(Console.ReadLine());
+
+        //math class in c#
+
+        float abc=9.99f;
+        //Console.WriteLine(Math.Round(abc));
+
+        float x=-4.55f;
+        Console.WriteLine(Math.Abs(x));
+    }
+}
